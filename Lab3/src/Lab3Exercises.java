ec1c9cb620b514a31f5019fb4c1ba2dfb80b5987
4015f18 <4015f18@136.145.57.226>
2017-12-05 10:54:32 -0400

    changing jframe name and size

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,15 @@
 import javax.swing.JFrame;
 
+
 public class Lab3Exercises {
+
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+
+		JFrame myFrame = new JFrame("Nashali Rivera");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(500, 170);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		//end of Q4
 	}
 }
