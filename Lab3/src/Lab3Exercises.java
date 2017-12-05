dfc9a691d4b93c99e449eb0db36883f40f1ac341
4015f18 <4015f18@136.145.57.226>
2017-12-05 11:07:42 -0400

    changed location for the center of screen

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -7,9 +7,13 @@ public class Lab3Exercises {
 
 		JFrame myFrame = new JFrame("Nashali Rivera");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(500, 170);
+		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
+	
+		
 		myFrame.setVisible(true);
 		//end of Q4
+		
+        
 	}
 }
